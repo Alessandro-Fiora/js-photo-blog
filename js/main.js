6edcd80bfb222da862a7dbc254aa0f0a547c52b3
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-07 13:07:56 +0100

    add  event listener

--- js/main.js
+++ js/main.js
@@ -3,33 +3,52 @@ const cardRowEl = document.getElementById("card-row");
 const cardNumber = 6;
 
 const fetchAPI = async () => {
-  let cardsHTML = "";
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/photos?_start=5_limit=${cardNumber}`
+    `https://jsonplaceholder.typicode.com/photos?_limit=${cardNumber}`
   );
   const photos = await response.json();
-  photos.forEach((photo) => {
-    cardsHTML += `
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="card h-100 rounded-0">
-                <img
-                src="${photo.url}"
-                alt=""
-                class="p-3 card-img-top img-fluid"
-                />
-                <div class="card-body">
-                    <p class="card-text">
-                    ${photo.title}
-                    </p>
-                </div>
-            </div>
-        </div>`;
-  });
-  cardRowEl.innerHTML = cardsHTML;
+  return photos;
 };
 
+const printPhotos = async (photos) => {
+  let cardsHTML = "";
+  photos.forEach((photo) => {
+    cardsHTML += `
+          <div class="col-12 col-md-6 col-lg-4">
+              <div class="card photo-card h-100 rounded-0">
+                  <img
+                  src="${photo.url}"
+                  alt=""
+                  class="p-3 card-img-top img-fluid"
+                  />
+                  <div class="card-body">
+                      <p class="card-text">
+                      ${photo.title}
+                      </p>
+                  </div>
+              </div>
+          </div>`;
+  });
+  cardRowEl.innerHTML = cardsHTML;
+
+  const cardsEL = await document.querySelectorAll(".photo-card");
+  console.log(cardsEL);
+
+  cardsEL.forEach((card) => {
+    card.addEventListener("click", () => {
+      alert("click");
+    });
+  });
+};
+
+const cardsEntry = async () => {
+  printPhotos(await fetchAPI());
+};
+
+cardsEntry();
+
 // fetch(
-//   `https://jsonplaceholder.typicode.com/photos?_start=5_limit=${cardNumber}`
+//   `https://jsonplaceholder.typicode.com/photos?_limit=${cardNumber}`
 // ).then((response) =>
 //   response.json().then((data) => {
 //     console.log(data);
